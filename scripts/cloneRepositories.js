0cf6975cfe219d3cefdd9fcf847d1ed61edf9925
gizeasy <gizeasy@gmail.com>
2023-05-19 15:29:56 +0300

    chore: configure build for windows

---
+++ scripts/cloneRepositories.js
@@ -0,0 +1,67 @@
+const { Command } = require('@oclif/command');
+const logSymbols = require('log-symbols');
+const { remove } = require('fs-extra');
+const { promisify } = require('util');
+
+const { exec } = require('child_process');
+
+const execAsync = promisify(exec);
+
+const repos = [
+  'portal',
+  'analytic-ui',
+  'uikit',
+  'charts',
+  'header',
+  'stats',
+  'react-big-calendar-adapter',
+  'gantt-task-react-adapter',
+  'rc-tree-adapter',
+  'rc-table-adapter',
+  'ag-grid-adapter',
+  'widgets',
+  'amcharts-map-examples',
+  'gpn-responses',
+  'icons',
+];
+
+const clone = async (repoName) => {
+  console.log(`cloning ${repoName}`);
+  await execAsync(
+    `git clone -b master https://github.com/consta-design-system/${repoName}.git ./repositories/${repoName}`,
+  );
+};
+
+class GenerateCommand extends Command {
+  async safeInvokeHook(hook) {
+    if (hook !== undefined) {
+      if (typeof hook === 'function') {
+        this.log(`start executing ${hook.name}`);
+        await hook();
+        this.log(`finish executing ${hook.name}`);
+      } else {
+        this.error(`${hook.name} should be function!`);
+      }
+    }
+  }
+
+  async run() {
+    const hrstart = process.hrtime();
+
+    try {
+      await remove('./repositories');
+      await Promise.all(repos.map((item) => clone(item)));
+    } catch (err) {
+      this.error(err);
+    }
+
+    const hrend = process.hrtime(hrstart);
+
+    this.log(logSymbols.success, `repositories is cloned!🔧💥`);
+    this.log(`Execution time: ${hrend[0]}s`);
+  }
+}
+
+GenerateCommand.description = 'Cloning repos...';
+
+GenerateCommand.run();
