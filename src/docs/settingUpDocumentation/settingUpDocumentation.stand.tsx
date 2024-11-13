36602b92e56cd0ce1ed168b05bb324dbd13dad96
alyonurchick1 <Alyonurchick1@yandex.ru>
2024-11-13 13:58:59 +0300

    docs(editorial policy): add setting up documentation
    edit documentation structure and add setting up documentation

---
+++ src/docs/settingUpDocumentation/settingUpDocumentation.stand.tsx
@@ -0,0 +1,8 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'Работа с документацией',
+  id: 'settin-up-documentation',
+  group: 'portal',
+  order: 840,
+});
