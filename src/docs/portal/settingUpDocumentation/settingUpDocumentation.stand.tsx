6e9748c4cb4b649c418e8b2d24468a7bb57d0925
alyonurchick1 <Alyonurchick1@yandex.ru>
2024-12-02 13:56:21 +0300

    add notes

--- src/docs/portal/settingUpDocumentation/settingUpDocumentation.stand.tsx
+++ src/docs/portal/settingUpDocumentation/settingUpDocumentation.stand.tsx
@@ -2,7 +2,7 @@ import { createStand } from '##/stand/standConfig';
 
 export default createStand({
   title: 'Работа с документацией',
-  id: 'settin-up-documentation',
+  id: 'setting-up-documentation',
   group: 'portal',
   order: 870,
 });
