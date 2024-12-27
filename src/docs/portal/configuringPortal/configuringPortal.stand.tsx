2f9f96af083dd47d8d6c58da87ed1c7b85483b56
alyonurchick1 <Alyonurchick1@yandex.ru>
2024-12-27 16:26:59 +0300

    edit setting up

--- src/docs/portal/configuringPortal/configuringPortal.stand.tsx
+++ src/docs/portal/configuringPortal/configuringPortal.stand.tsx
@@ -2,7 +2,7 @@ import { createStand } from '##/stand/standConfig';
 
 export default createStand({
   title: 'Работа с документацией',
-  id: 'setting-up-documentation',
+  id: 'configuring-portal',
   group: 'portal',
   order: 870,
 });
